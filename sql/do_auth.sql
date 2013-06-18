4b99adbe3519c09d9993a7376f6946caff8e1222
Lee Carmichael <lecar_red@yahoo.com>
2013-06-18 09:37:22 -0500

    added db to auth

--- sql/do_auth.sql
+++ sql/do_auth.sql
@@ -1,4 +1,4 @@
 -- TODO: make readonly user
 CREATE USER 'brew_finder'@'localhost' IDENTIFIED BY 'bf';
-GRANT ALL PRIVILEGES ON *.* TO 'brew_finder'@'localhost' 
+GRANT ALL PRIVILEGES ON brew_finder.* TO 'brew_finder'@'localhost' 
 	WITH GRANT OPTION;
